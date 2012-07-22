a655c8fadf4f717ba2ace9227bdefaf0ebe9c426
hokorobi <hokorobi.hokorobi@gmail.com>
2012-07-22 17:23:20 +0900

    change plugin twicli_post_canceler.js: ESC ????????????????????

--- plugins/twicli_post_canceler.js
+++ plugins/twicli_post_canceler.js
@@ -7,18 +7,31 @@
 // @twitter https://twitter.com/azu_re
 // ==/UserScript==
 /* http://gist.github.com/567983.txt をプラグインにロード */
+
+// オリジナルからの変更点。
+// - esc キーでキャンセルできるように。
+// - キャンセル可能な残り時間を出すように。
+
 (function(){
 var DELAY_TIME = 5000; //デフォルト5秒
-    
-var addEvent; //変数を宣言
+
+var addEvent, removeEvent; //変数を宣言
 if (document.addEventListener) {
     //addEventListenerが使える場合(IE以外)
     addEvent = function(node, type, handler) {
         node.addEventListener(type, handler, false);
     };
+    removeEvent = function(node, type, handler) {
+        node.removeEventListener(type, handler, false);
+    };
 } else if (document.attachEvent) { // IE用
     addEvent = function(node, type, handler) {
         node.attachEvent('on' + type, function(evt) {
+            handler.call(node, evt);
+        });
+    };
+    removeEvent = function(node, type, handler) {
+        node.detachEvent('on' + type, function(evt) {
             handler.call(node, evt);
         });
     };
@@ -29,12 +42,23 @@ var postCancel = {
     append: function(timeID) {
         var button = document.createElement("button");
         button.id = postCancel.id;
-        button.setAttribute("style", "position:fixed;top:1px;right:1px;box-shadow: 0 1px 5px #0061aa, inset 0 10px 20px #b6f9ff;-webkit-box-shadow: 0 1px 5px #0061aa, inset 0 10px 20px #b6f9ff;-moz-box-shadow: 0 1px 5px #0061aa, inset 0 10px 20px #b6f9ff;width: 100px;margin: 5px;padding: 0;background: #6fb2e5;z-index:333;")
-        addEvent(button , "click", function() {
-            window.clearTimeout(timeID);
-            postCancel.remove();
-        }, false);
-        button.appendChild(document.createTextNode("送信キャンセル"));
+        button.setAttribute("style", "position:fixed;top:0;right:0;width: 5em;margin: 5px;padding: 10px;z-index:333;");
+        function cancel(e) {
+            var keyCode = (e.keyCode || e.charCode || e.which);
+            if (!keyCode || keyCode === 27) {
+                window.clearTimeout(timeID);
+                e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+                removeEvent($('fst'), 'keypress', cancel);
+                postCancel.remove();
+            }
+        }
+        addEvent(button , "click", cancel);
+        addEvent($('fst'), 'keypress', cancel);
+        var time_end = DELAY_TIME + (+new Date);
+        setTimeout(function(){
+            button.innerHTML = Math.floor((time_end - (+new Date)) / 100) / 10;
+            setTimeout(arguments.callee, 50);
+        }, 50);
         document.body.appendChild(button);
     },
     remove: function() {
@@ -48,6 +72,7 @@ function addDelayforPress(before, msec) {
     window.press = function(e) {
         if (e != 1) key_press_detected = true;
         if (e != 1 && (e.keyCode != 13 && e.keyCode != 10 || !decr_enter && (e.ctrlKey || e.shiftKey) || decr_enter && !(e.ctrlKey || e.shiftKey))) return true;
+        e.preventDefault && e.preventDefault();
         var st = document.frm.status;
         if (st.value == '') {
             $("loading").style.display = "block";
