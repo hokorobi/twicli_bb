f18f9277e5f479eb27e47ae26c641ade2132a540
hokorobi <hokorobi.hokorobi@gmail.com>
2013-09-16 16:33:03 +0900

    plugins/thumbnail.js: Support png, jpg, gif
    ??????????????????????????
    ??????????????
    ??????? pic.non117.com, cache.gyazo.com

--- plugins/thumbnail.js
+++ plugins/thumbnail.js
@@ -122,17 +122,14 @@ registerPlugin({
 							addThumbnail(elem, x.response.post.thumb_url, url);
 					});
 		}
-		else if (url.match(/^http:\/\/pic\.non117\.com/)) {
-			addThumbnail(elem, url, url);
-		}
 		else if (url.match(/^http:\/\/miil\.me\/p\//)) {
 			addThumbnail(elem, url + '.jpeg?size=240', url);
 		}
-		else if (url.match(/^http:\/\/cache\.gyazo\.com/)) {
-			addThumbnail(elem, url, url);
-		}
 		else if (url.match(/^http:\/\/gifzo\.net/)) {
 			addThumbnail(elem, url + '.gif' , url);
+		}
+		else if (url.match(/(\.png|\.jpg|\.gif)$/)) {
+			addThumbnail(elem, url, url);
 		}
 	}
 });
